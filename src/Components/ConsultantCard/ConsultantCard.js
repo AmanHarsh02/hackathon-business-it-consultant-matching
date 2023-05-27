8a00d563b06d988642fdde478e6cdebb95545147
Wadad Parker <117439709+WadadParker@users.noreply.github.com>
2023-05-27 21:34:36 +0000

    style: consutan cart

--- src/Components/ConsultantCard/ConsultantCard.js
+++ src/Components/ConsultantCard/ConsultantCard.js
@@ -9,8 +9,8 @@ export const ConsultantCard = ({consultantId,overallRating,budgetRating}) => {
   const shortListCandidate = () => {
     dispatch({type:"CONSULTANTS_SHORTLIST",payload:{consultantId,overallRating,budgetRating}})
   }
-  return (
-    <div className="consultant__card">
+  return (<>
+    {/* <div className="consultant__card"> 
       <div className="consultant__avatar">
         {`${consultantCardData?.empName.split(" ")[0][0]} ${consultantCardData.empName.split(" ")[1][0]}`}
       </div>
@@ -24,8 +24,38 @@ export const ConsultantCard = ({consultantId,overallRating,budgetRating}) => {
         <p>{consultantCardData?.manDayCost}</p>
         <button onClick={shortListCandidate} className="shortlist__btn">Shortlist +</button>
       </div>
+    </div> */}
+    <div className="consultant-container">
+      <div className="head-container">
+        <img src="https://e1.pxfuel.com/desktop-wallpaper/429/293/desktop-wallpaper-coque-et-etui-iphone-luffy-smiling-zoro-aesthetic-ps4-thumbnail.jpg" alt="" widht={135} height={150}/>
+        <div className="match-container">
+          <p>70%</p>
+          <small>Match Score</small>
+        </div>
+
+        <div className="main-container">
+          <b>Company Name</b>
+          <p>This is the favorite company of all time</p>
+        </div>
+
+        <div className="footer-container">
+          <button>Shortlist</button>
+          <div className="price-container">
+            <strong>100000Rs</strong>
+            <small>Effort Estimate</small>
+          </div>
+        </div>
+      </div>
+
     </div>
+    </>
   );
 };
 
 // [{consultantId,overallRating,budgetRating}]
+// lin 13full card container
+// line 14 avatar: image (JD)
+// line 17 consultant k details, Name n descrition
+// line 25 short list ka button
+// together with stars
+// line 20 container is match score container, it was circle, now its clean number
